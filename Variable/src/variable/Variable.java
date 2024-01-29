759c3df16a09f8edbaa27d346a633dcd909aaee7
Ekasetyo <setyoeka07@gmail.com>
2024-01-29 14:17:51 +0700

    Variable Java
    Belajar Variabel atau nama pada java dengan netbeans

---
+++ Variable/src/variable/Variable.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package variable;
+
+/**
+ *
+ * @author acer
+ */
+
+
+public class Variable {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 24;
+        int UMUR = 25;
+        
+        System.out.println("umur =" + umur);
+        System.out.println("UMUR" + UMUR);
+    }
+    
+}
